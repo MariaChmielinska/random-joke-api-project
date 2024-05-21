38ee596e9fba84f666b99117f5aaf050aa01adac
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-05-21 19:29:48 +0200

    con version get query

--- app.js
+++ app.js
@@ -29,3 +29,37 @@ button.addEventListener('click', async function () {
 // 2. En la página Web de la API, existe documentación con los campos y estructura de datos que devuelve la API
 
 // 3. No usar el ejercicio de chuck norris como base. Esta bien mirar como se hace el fetch en otros ejercicios. No copiar y pegar, escribirlo todo para que se vaya quedando e interorizando
+
+/*
+REcordar que deberiamos añadir en el HTML:     <div>
+        <label for="keyword">Search by text: </label>
+        <input type="text" name="keyword" id="keyword">
+    </div>
+
+    Modificar el string de la URL en función de lo que pone el usuario // Tu códgigo aquí
+// Tu códgigo aquí
+const image = document.querySelector('img');
+const jokeDIV = document.querySelector('#display-joke');
+const button = document.querySelector('#get-joke');
+const keyword = document.querySelector('#keyword');
+
+button.addEventListener('click', async function () {
+
+    // 1. Hacer una petición GET a la API que nos ofrece el chiste de Chuck Norris. Existe un método nativo de JavaScript que se llama 'fetch'. En su versión más simple, simplemente le tenemos que pasar como parámetro, la URL de la API que queremos consumir. El método fetch devuelve la respuesta de la API
+
+    // Recupero el texto que ha introducido el usuario
+    const textToSearch = keyword.value;
+    console.log("🚀 ~ file: app.js:14 ~ textToSearch:", textToSearch)
+
+    const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${textToSearch}`);
+    console.log("objeto response", response);
+    // 2. Converir el JSON que nos devuelve la API a un tipo de dato que entienda JavaScript (típicamente, será un objeto o un array de objetos)
+    const data = await response.json();
+    console.log("objeto data", data);
+
+
+    // // 3. Modificar el DOM para mostrar el chiste al usuario
+    jokeDIV.textContent = data.result[0].value;
+
+});
+
